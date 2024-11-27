8f3cb0267225e72eeda57bd670805d994ab5f714
0xtter <acc.0xtter@gmail.com>
2024-11-27 13:24:56 +0100

    Change script + add scripts backup

--- create_dotfile_backup.sh
+++ create_dotfile_backup.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-BACKUP_FOLDER="$(dirname $(realpath "$0"))/config"
+# Define backup base directories
+SCRIPT_DIR="$(dirname $(realpath "$0"))"
+BACKUP_FOLDER="$SCRIPT_DIR"
+
+# Define source directories
 DOTFILES_FOLDER=$(realpath ~/.config)
+LOCAL_FOLDER=$(realpath ~/.local)
 
-FOLDERS_TO_BACKUP=( "$DOTFILES_FOLDER/cava" "$DOTFILES_FOLDER/dolphinrc" "$DOTFILES_FOLDER/kitty" "$DOTFILES_FOLDER/nautilus" "$DOTFILES_FOLDER/rofi" "$DOTFILES_FOLDER/hypr" )
+# Define the folders and files to backup with their base directories
+FILES_TO_BACKUP=(
+    "$DOTFILES_FOLDER/cava"
+    "$DOTFILES_FOLDER/dolphinrc"
+    "$DOTFILES_FOLDER/kitty"
+    "$DOTFILES_FOLDER/nautilus"
+    "$DOTFILES_FOLDER/rofi"
+    "$DOTFILES_FOLDER/hypr"
+    "$LOCAL_FOLDER/bin/play-previous"
+    "$LOCAL_FOLDER/bin/play-next"
+    "$LOCAL_FOLDER/bin/spotify-pause"
+    "$LOCAL_FOLDER/bin/player-pause"
+)
 
-for file in "${FOLDERS_TO_BACKUP[@]}"; do
-    echo "$file"
-    rsync -vaP "$file" "$BACKUP_FOLDER"
+# Backup the files
+for file in "${FILES_TO_BACKUP[@]}"; do
+    echo "Backing up $file"
+    rsync -vaP --relative "$file" "$BACKUP_FOLDER"
 done
